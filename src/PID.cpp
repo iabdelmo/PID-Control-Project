e04c3702c724491413527824e71ae710fe7a4a03
muhammad-nassef <muhamad.nassef@gmail.com>
2017-09-03 16:57:27 +0200

    feat: Apply PID controller with fixed gain values
    PID Controller is applied with fixed values for Kp, Ki and Kd to test the car performance and see the effect of each gain

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,32 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    PID::p_error = 0.0;
+    PID::i_error = 0.0;
+    PID::d_error = 0.0;
+
 }
 
 void PID::UpdateError(double cte) {
+
+    d_error = cte - p_error;
+
+    p_error = cte;
+
+    i_error += cte;
+
+
 }
 
 double PID::TotalError() {
+
+    double total_error = -Kp * p_error - Kd * d_error - Ki * i_error;
+
+    return total_error;
 }
 
